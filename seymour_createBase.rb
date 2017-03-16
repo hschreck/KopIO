a5410285f3bdaef27604fe6068028d7b7cbdc151
Harold Schreckengost <harold@haroldmschreckengost.com>
2017-03-16 12:49:15 -0500

    ...

--- seymour_createBase.rb
+++ seymour_createBase.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
-IMAGELOCATION = "/home/partimag/".freeze
+IMAGELOCATION = "/home/partimag/"
 STOPCHAR = "S"
 SIZES = [80, 120, 160, 250, 320, 256, 500, 1000].freeze
 
@@ -7,7 +7,7 @@ driveSize = `lsblk -b | grep "sda " | grep -oE '[0-9]{3,}'`.chomp.to_i
 humanReadableSize = driveSize / 1000.0 / 1000 / 1000
 humanReadableSize = SIZES.map { |x| [x, (x - humanReadableSize).abs] }.to_h.min_by { |_size, distance| distance }[0]
 
-location = IMAGELOCATION.to_s
+location << IMAGELOCATION
 selection = ""
 until selection == STOPCHAR do
   directory = Dir.entries(location)
